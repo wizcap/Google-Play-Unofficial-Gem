f5b77294698725403994ff013ef4cc88d845e0c4
Tom Doyle <me@tdoyle.me>
2012-03-07 00:40:22 -0800

    Removed DS_Store and blocked in .gitignore

--- lib/google-play.rb
+++ lib/google-play.rb
@@ -1,12 +1,22 @@
 require 'open-uri'
 require 'nokogiri'
 require './google-play-class.rb'
+require "awesome_print"
 
 module GooglePlay
+  def self.search(query, type)
+  	returnarray = ["query" => query, "type" => type]
+		doc = Nokogiri::HTML(open("https://play.google.com/store/search?q=#{query}&c=#{type}"))
+		doc.css('li.search-results-item').each{ |links|
+			temparray = ["title" => links.css('a.title').text]
+			returnarray << temparray
+		}
+		return returnarray
+  end
   def self.info(id, type)
 		doc = Nokogiri::HTML(open("https://play.google.com/store/#{type}/details?id=#{id}"))
 		return PlayResult.new(id,type,doc.css('h1.doc-banner-title').text)
  	end
 end
 
-#usage GooglePlay.info("B2bigmdzursbckmnhfgmjdwscyy", "music/album")
+ap GooglePlay.info("B2bigmdzursbckmnhfgmjdwscyy", "music/album")
